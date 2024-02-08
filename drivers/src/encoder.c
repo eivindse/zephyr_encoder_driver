68291d8db6fd7a3a59770fbb5e7fc00b58e1eb0e
Eivind Strand Erichsen <ese@datarespons.no>
2024-02-08 12:26:07 +0100

    Everything fix

--- drivers/src/encoder.c
+++ drivers/src/encoder.c
@@ -2,13 +2,16 @@
 
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/logging/log.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 
 #include <helpers/nrfx_gppi.h>
+#include <nrfx_ppi.h>
 #include <nrfx_gpiote.h>
 #include <nrfx_timer.h>
 
+LOG_MODULE_REGISTER(Encoder_Counter, LOG_LEVEL_INF);
 
 #if DT_HAS_COMPAT_STATUS_OKAY(DT_DRV_COMPAT)
 
@@ -30,8 +33,7 @@ static int32_t get_timer_count(const struct device* dev)
     const struct encoder_config* config     = dev->config;
     nrfx_timer_t                 timer_inst = config->timer_inst;
 
-    // Capture count value in TIMER CC register.
-    // Returns count but ignore it because it is recommended to use value from nrfx_timer_capture_get instead
+    // Capture count and return count, but it is recommended to get value with xx_get when using ppi
     nrfx_timer_capture(&timer_inst, NRF_TIMER_CC_CHANNEL0);
 
     // get count value from TIMER CC register
@@ -43,59 +45,64 @@ static int encoder_init(const struct device* dev)
     const struct encoder_config* config     = dev->config;
     nrfx_timer_t                 timer_inst = config->timer_inst;
     nrfx_gpiote_pin_t            pin        = config->gpio_spec.pin;
+    
+    uint8_t gpiote_chan;
+    nrfx_err_t err = nrfx_gpiote_channel_alloc(&gpiote_chan);
+    if (err != NRFX_SUCCESS) {
+        return err;
+    }
 
-    const struct gpio_dt_spec    pin_spec   =
-    {
-        .pin = config->gpio_spec.pin,
+    const nrfx_gpiote_trigger_config_t trigger_cfg = {
+        .trigger = NRFX_GPIOTE_TRIGGER_TOGGLE,
+        .p_in_channel = &gpiote_chan,
     };
 
-    gpio_pin_configure_dt(&pin_spec, GPIO_INPUT);
+    const nrfx_gpiote_input_config_t input_config = {
+        .pull = NRF_GPIO_PIN_NOPULL,
+    };
 
-    // allocate available channel
-    uint8_t gpiote_chan;
-    nrfx_gpiote_channel_alloc(&gpiote_chan);
+    LOG_INF("nrfx_gpiote_input_configure");
+    // Function for configuring the specified input pin and input event
+    err = nrfx_gpiote_input_configure(pin, &input_config, &trigger_cfg, NULL);
+    if (err != NRFX_SUCCESS) {
+        return err;
+    }
 
-
-    // const uint8_t * const gppi_ch;
-	static const nrfx_gpiote_trigger_config_t trigger_cfg = {
-		.trigger = NRFX_GPIOTE_TRIGGER_TOGGLE,
-	};
-
-	static const nrfx_gpiote_input_config_t input_config = {
-		.pull = NRF_GPIO_PIN_NOPULL,
-	};
-
-    nrfx_err_t res = nrfx_gpiote_input_configure(pin, &input_config, &trigger_cfg, NULL);
-
-    // GPIOTE EVENT trigger for ppi. false - no irq
+    // GPIOTE EVENT trigger for ppi
     nrfx_gpiote_trigger_enable(pin, false);
 
-    // Initialize the timer in counter mode
-    nrfx_timer_config_t timer_cfg = NRFX_TIMER_DEFAULT_CONFIG(NRF_TIMER_FREQ_16MHz);
-    timer_cfg.mode                = NRF_TIMER_MODE_COUNTER;
-    res                           = nrfx_timer_init(&timer_inst, &timer_cfg, NULL);
-    if (res != NRFX_SUCCESS) {
-        return res;
+	const nrfx_timer_config_t timer_cfg = {
+		.frequency = NRFX_MHZ_TO_HZ(1UL),
+		.mode = NRF_TIMER_MODE_COUNTER,
+		.bit_width = NRF_TIMER_BIT_WIDTH_32,
+		.interrupt_priority = NRFX_TIMER_DEFAULT_CONFIG_IRQ_PRIORITY,
+		.p_context = NULL,
+	};
+
+    err = nrfx_timer_init(&timer_inst, &timer_cfg, NULL);
+    if (err != NRFX_SUCCESS) {
+        return err;
     }
 
+    nrf_ppi_channel_t  ppi_chan;
+    err = nrfx_ppi_channel_alloc(&ppi_chan);
+
     // connect gpiote event to timer task through channel
-    res = nrfx_ppi_channel_assign(gpiote_chan, nrfx_gpiote_in_event_address_get(pin),  nrfx_timer_task_address_get(&timer_inst, NRF_TIMER_TASK_COUNT));
-    if (res != NRFX_SUCCESS) {
-        return res;
+    err = nrfx_ppi_channel_assign(ppi_chan, nrfx_gpiote_in_event_address_get(pin), nrfx_timer_task_address_get(&timer_inst, NRF_TIMER_TASK_COUNT));
+    if (err != NRFX_SUCCESS) {
+        return err;
     }
+    nrf_ppi_channels_enable(NRF_PPI, BIT(ppi_chan));
 
     nrfx_timer_enable(&timer_inst);
 
+    LOG_INF("encoder initialized");
     return 0;
 }
 
 #define ENCODER_DEFINE(i)                                                                                           \
     static const struct encoder_config encoder_config_##i = {                                                       \
-        .timer_inst = {                                                                                             \
-            .p_reg            = (NRF_TIMER_Type*)DT_PROP(DT_INST(i, encoder_counter), timer_address),               \
-            .instance_id      = DT_PROP(DT_INST(i, encoder_counter), timer_id),                                     \
-            .cc_channel_count = 4,                                                                                  \
-        },                                                                                                          \
+        .timer_inst = NRFX_TIMER_INSTANCE(CONFIG_ENCODER##i##_TIMER),                                               \
         .gpio_spec = GPIO_DT_SPEC_INST_GET_OR(i, gpios, { 0 }),                                                     \
     };                                                                                                              \
     static int get_and_reset_count##i(const struct device* dev, enum sensor_channel chan, struct sensor_value* val) \
--- src/main.c
+++ src/main.c
@@ -1,51 +1,54 @@
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(Example_App, LOG_LEVEL_INF);
 
 static struct sensor_value encoder_left_data;
 static struct sensor_value encoder_right_data;
 
-static const struct device* encoder_left_dev  = DEVICE_DT_GET(DT_NODELABEL(encoder_left));
+static const struct device* encoder_left_dev = DEVICE_DT_GET(DT_NODELABEL(encoder_left));
 static const struct device* encoder_right_dev = DEVICE_DT_GET(DT_NODELABEL(encoder_right));
 
-void get_encoder_data(){
+void get_encoder_data()
+{
     int ret;
     ret = sensor_channel_get(encoder_left_dev, SENSOR_CHAN_POS_DY, &encoder_left_data);
     if (ret != 0) {
-        printk("sensor_channel_get error: %d", ret);
+        LOG_INF("sensor_channel_get error: %d", ret);
     }
 
     ret = sensor_channel_get(encoder_right_dev, SENSOR_CHAN_POS_DY, &encoder_right_data);
     if (ret != 0) {
-        printk("sensor_channel_get error: %d", ret);
+        LOG_INF("sensor_channel_get error: %d", ret);
     }
 }
 int main(void)
 {
     if (encoder_left_dev == NULL || encoder_left_dev == NULL) {
-        printk("ERROR: encoder not found");
+        LOG_INF("ERROR: encoder not found");
         return -1;
     }
     if (!device_is_ready(encoder_left_dev)) {
-        printk("left encoder not ready");
+        LOG_INF("left encoder not ready");
     } else {
-        printk("left encoder ready");
+        LOG_INF("left encoder ready");
     }
     if (!device_is_ready(encoder_right_dev)) {
-        printk("right encoder not ready");
+        LOG_INF("right encoder not ready");
     } else {
-        printk("right encoder ready");
+        LOG_INF("right encoder ready");
     }
 
-    while (1)
-    {
+    while (1) {
         int ret;
         ret = sensor_channel_get(encoder_left_dev, SENSOR_CHAN_ALL, &encoder_left_data);
         ret = sensor_channel_get(encoder_right_dev, SENSOR_CHAN_ALL, &encoder_right_data);
-        printk("encoder left data %d", encoder_left_data.val1);
-        printk("encoder right data %d", encoder_right_data.val1);
+        LOG_INF("encoder left data %d", encoder_left_data.val1);
+        LOG_INF("encoder right data %d", encoder_right_data.val1);
         k_msleep(1000); // Update heading at least every time we get gyro measurments which is 12.5 Hz.
     }
-    
+
     return 0;
 }
